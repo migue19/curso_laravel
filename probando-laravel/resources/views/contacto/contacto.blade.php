9ee49a3cb3534649e6ad60faea2b5f44c9f84cb0
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2019-02-11 07:01:21 -0600

    add routes and contacts file

---
+++ probando-laravel/resources/views/contacto/contacto.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contacto</title>
+</head>
+<body>
+   <h1>Contacto {{$nombre}}, edad: {{$edad}} </h1>
+</body>
+</html>
--- probando-laravel/routes/web.php
+++ probando-laravel/routes/web.php
@@ -14,3 +14,29 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/hola-mundo',function(){
+	return 'Hola Mundo!!, Soy Miguel Mexicano';
+});
+
+
+/*Route::get('/contacto',function(){
+	return view('contacto');
+});*/
+
+Route::get('/contacto/{nombre?}/{edad?}',function($nombre = "Miguel",$edad = 26){
+	/*return view('contacto',array(
+		"nombre" => $nombre,
+		"edad" => $edad
+	));*/
+
+	//puede ser con . o /
+	return view('contacto/contacto')
+			->with('nombre',$nombre)
+			->with('edad',$edad);
+
+})->where([
+	'nombre' => '[A-Za-z]+',
+	'edad' => '[0-9]+'
+]);
